1e1a85fe79bfc51576d07c601efc4b27d38e8acc
franciscoBascur <fr.bascur@duocuc.cl>
2025-06-27 14:34:09 -0400

    actualizacion microservicio tutor

--- src/main/java/com/edutech/cl/edutech/controller/TutorController.java
+++ src/main/java/com/edutech/cl/edutech/controller/TutorController.java
@@ -1,55 +1,16 @@
 package com.edutech.cl.edutech.controller;
 
-import com.edutech.tutor.model.Tutor;
-import com.edutech.tutor.service.TutorService;
-import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/tutores")
 public class TutorController {
 
-    private final TutorService tutorService;
-
-
-
-
-    // Obtener tutor por ID
-    @GetMapping("/{id}")
-    public ResponseEntity<Tutor> obtener(@PathVariable Long id) {
-        return tutorService.obtenerPorId(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    @GetMapping("/tutores")
+    public List<String> getTutores() {
+        return List.of("Tutor 1", "Tutor 2", "Tutor 3");
     }
 
-    // Crear nuevo tutor
-    @PostMapping
-    public Tutor crear(@Valid @RequestBody Tutor tutor) {
-        return tutorService.guardar(tutor);
-    }
-
-    // Actualizar tutor
-    @PutMapping("/{id}")
-    public ResponseEntity<Tutor> actualizar(@PathVariable Long id, @Valid @RequestBody Tutor nuevo) {
-        return tutorService.obtenerPorId(id)
-                .map(tutor -> {
-                    tutor.setNombre(nuevo.getNombre());
-                    tutor.setCorreo(nuevo.getCorreo());
-                    tutor.setEspecialidad(nuevo.getEspecialidad());
-                    return ResponseEntity.ok(tutorService.guardar(tutor));
-                }).orElse(ResponseEntity.notFound().build());
-    }
-
-    // Eliminar tutor
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
-        if (tutorService.obtenerPorId(id).isPresent()) {
-            tutorService.eliminar(id);
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
-    }
 }
