53f7f8c8aaf4513dd2d0b047fc069b8d1ec9421c
franciscoBascur <fr.bascur@duocuc.cl>
2025-06-27 14:44:44 -0400

    actualizacion 0.2 microservicio tutor

--- src/main/java/com/edutech/cl/edutech/controller/TutorController.java
+++ src/main/java/com/edutech/cl/edutech/controller/TutorController.java
@@ -3,14 +3,24 @@ package com.edutech.cl.edutech.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class TutorController {
+
+    private final List<String> tutores = new ArrayList<>(List.of("Tutor 1", "Tutor 2", "Tutor 3"));
 
     @GetMapping("/tutores")
     public List<String> getTutores() {
-        return List.of("Tutor 1", "Tutor 2", "Tutor 3");
+        return tutores;
     }
 
+    @PostMapping("/tutores")
+    public String agregarTutor(@RequestBody String nuevoTutor) {
+        tutores.add(nuevoTutor);
+        return "Tutor agregado: " + nuevoTutor;
+    }
 }
